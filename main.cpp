c8def2c6d3f16e2bdb233dcdc96ba50abbd0514d
Alif <mail@ahmedalif.com>
2020-07-23 21:16:32 +0600

    Artwork done successfully

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void myInit(void)
 glClearColor(240.0f/255.0f, 241.0f/255.0f, 227.0f/255.0f, 1.0f);
 
 glMatrixMode(GL_PROJECTION);
-gluOrtho2D(0.0, 512.0, 0.0, 512.0);
+gluOrtho2D(0.0, 550.0, 0.0, 550.0);
 }
 void myDisplay(void)
 {
@@ -16,14 +16,10 @@ glLineWidth(6.0);
 
 //coordinate anti-clockwise
 
-//rgb(253,173,114)
-//rgb(255,136,73)
-
-
 //1st
 
 glBegin(GL_TRIANGLES);
-glColor3f(253.0f/255.0f, 173.0f/255.0f, 114.0f/255.0f);
+glColor3f(254.0f/255.0f, 169.0f/255.0f, 108.0f/255.0f);
 
 
 glVertex2i(460,310); //right
@@ -51,7 +47,7 @@ glBegin(GL_TRIANGLES);
 glColor3f(255.0f/255.0f, 147.0f/255.0f, 72.0f/255.0f);
 
 glVertex2i(340,390); //left
-glVertex2i(255,500); //top
+glVertex2i(250,512);  //top
 glVertex2i(280,371); //right
 
 
@@ -63,11 +59,10 @@ glFlush();
 glBegin(GL_QUADS);
 glColor3f(180.0f/255.0f, 86.0f/255.0f, 39.0f/255.0f);
 
-
-glVertex2i(280,371); //right bottom
-glVertex2i(255,500); //top
-glVertex2i(233,430); //upper left
-glVertex2i(233,378); //left bottom
+glVertex2i(280,371); //bottom right
+glVertex2i(250,512); //top
+glVertex2i(235,430); //upper left
+glVertex2i(233,378); //bottom left
 
 glEnd();
 glFlush();
@@ -75,18 +70,18 @@ glFlush();
 //5th
 
 glBegin(GL_QUADS);
-glColor3f(254.0f/255.0f, 177.0f/255.0f, 89.0f/255.0f);
+glColor3f(254.0f/255.0f, 174.0f/255.0f, 108.0f/255.0f);
 
-glVertex2i(188,315);
-glVertex2i(226,305);
-glVertex2i(280,371); //top left
-glVertex2i(233,378); //bottom left
+glVertex2i(188,315); //bottom left
+glVertex2i(226,305); //bottom right
+glVertex2i(280,371); //top right
+glVertex2i(233,378); //top left
 
 
 glEnd();
 glFlush();
 
-//6th  (255,184,100)
+//6th
 
 glBegin(GL_TRIANGLES);
 glColor3f(255.0f/255.0f, 155.0f/255.0f, 52.0f/255.0f);
@@ -105,7 +100,7 @@ glColor3f(255.0f/255.0f, 184.0f/255.0f, 100.0f/255.0f);
 
 glVertex2i(226,305); //top
 glVertex2i(290,180); //bottom
-glVertex2i(205,215); //left
+glVertex2i(226,210); //left
 
 glEnd();
 glFlush();
@@ -115,8 +110,8 @@ glFlush();
 glBegin(GL_QUADS);
 glColor3f(180.0f/255.0f, 86.0f/255.0f, 39.0f/255.0f);
 
-glVertex2i(188,315); //right
-glVertex2i(226,305); //top left
+glVertex2i(188,315); //top left
+glVertex2i(226,305); //top right
 glVertex2i(198,280); //bottom right
 glVertex2i(173,295); //bottom left
 
@@ -154,15 +149,81 @@ glFlush();
 glBegin(GL_TRIANGLES);
 glColor3f(255.0f/255.0f, 192.0f/255.0f, 138.0f/255.0f);
 
-glVertex2i(188,315);
-glVertex2i(205,215);
-glVertex2i(198,280);
+glVertex2i(226,305); //top right
+glVertex2i(198,280); //top left
+glVertex2i(226,210); //bottom
 
+glEnd();
+glFlush();
+
+
+//12th
+
+glBegin(GL_QUADS);
+glColor3f(255.0f/255.0f, 192.0f/255.0f, 138.0f/255.0f);
+
+glVertex2i(226,210); //bottom right
+glVertex2i(198,280); //top right
+glVertex2i(132,170); // bottom left
+glVertex2i(156,160); //bottom
+
+glEnd();
+glFlush();
+
+
+//13th
+
+glBegin(GL_QUADS);
+glColor3f(255.0f/255.0f, 170.0f/255.0f, 56.0f/255.0f);
+
+glVertex2i(233,378); //top
+glVertex2i(235,430); //top bottom
+glVertex2i(178,344); //bottom top
+glVertex2i(188,315); //bottom
+
+glEnd();
+glFlush();
+
+
+//14th
+
+glBegin(GL_TRIANGLES);
+glColor3f(255.0f/255.0f, 170.0f/255.0f, 56.0f/255.0f);
+
+glVertex2i(188,315); //bottom
+glVertex2i(178,344); //bottom top
+glVertex2i(173,295); //bottom left
+
+glEnd();
+glFlush();
+
+
+//15th
+
+glBegin(GL_TRIANGLES);
+glColor3f(180.0f/255.0f, 86.0f/255.0f, 39.0f/255.0f);
+
+glVertex2i(235,430);//bottom right
+glVertex2i(190,488); //top
+glVertex2i(199,374); //bottom left
 
 
 glEnd();
 glFlush();
 
+
+//16th
+
+glBegin(GL_TRIANGLES);
+glColor3f(250.0f/255.0f, 145.0f/255.0f, 84.0f/255.0f);
+
+glVertex2i(241,463);//right
+glVertex2i(190,488); //top
+glVertex2i(235,430); //bottom
+
+
+glEnd();
+glFlush();
 
 
 
@@ -174,7 +235,7 @@ glutInit(&iArgc, cppArgv); // initialize the toolkit
 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // set the display mode
 glutInitWindowSize(640,480); // set window size
 glutInitWindowPosition(100, 150); // set window upper left corner position on screen
-glutCreateWindow("My First Attempt"); // open the screen window (Title: My First Attempt)
+glutCreateWindow("Wolf Artwork"); // open the screen window (Title: My First Attempt)
 // register the callback functions
 glutDisplayFunc(myDisplay);
 myInit(); // additional initializations as necessary
